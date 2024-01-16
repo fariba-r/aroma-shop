16cb5bbb736993928be25594e8d5a96049c4003f
fariba_r <faribarezaee321@gmail.com>
2024-01-17 03:20:52 +0330

    chore: create item model

--- myapps/product/models.py
+++ myapps/product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+class Status(models.Model):
+    is_deleted = models.BooleanField()
+    created_at = models.DateTimeField(auto_now=True)
+    creator = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+class Item(Status):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    category_id = models.ForeignKey(Category)
+    detail_id =  models.ForeignKey(Detail)
+    count = models.IntegerField()
+    price = models.FloatField()
+    
