a60d7bc12439c6c2f681ae3064f00ac3d600ce86
fariba_r <faribarezaee321@gmail.com>
2024-01-24 02:42:05 +0330

    chore: fix register template

--- myapps/member/urls.py
+++ myapps/member/urls.py
@@ -7,6 +7,7 @@ from django.contrib.auth.views import LogoutView
 app_name = 'member'
 urlpatterns = [
     path('login/', views.LoginPassView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page=reverse_lazy('core:home')),name='logout')
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('core:home')),name='logout'),
+    path('singup/', views.SignUpView.as_view(), name='signup'),
 
 ]
--- myapps/member/views.py
+++ myapps/member/views.py
@@ -3,6 +3,9 @@ from django.views.generic import ListView, View, CreateView, TemplateView, FormV
 from django.contrib.auth.views import LoginView
 from django.urls import reverse, reverse_lazy
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 # Create your views here.
 
 class LoginPassView(LoginView):
@@ -16,5 +19,12 @@ class LoginPassView(LoginView):
         messages.error(self.request,'Invalid username or password')
         return self.render_to_response(self.get_context_data(form=form))
     
+
+class SignUpView(SuccessMessageMixin,CreateView):
+    form_class = UserCreationForm
+    template_name ="member/signup.html"
+    success_url = reverse_lazy('member:login')
+    success_message = "Your account was created successfully"
+
 class LoginEmailView():
     pass
