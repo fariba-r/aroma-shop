1ff721c224ccfa84c22c2c3c7506eecc273d6b1e
fariba_r <faribarezaee321@gmail.com>
2024-01-25 10:58:33 +0330

    chore:complete login with email frontend.with add js file

--- myapps/product/views.py
+++ myapps/product/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView, View, CreateView, TemplateView, FormView, UpdateView
 from django.shortcuts import get_object_or_404
+from django.urls import reverse, reverse_lazy
 from .models import *
 # Create your views here.
 class CategoryView(View):
     def get(self, request, *args, **kwargs):
         category = Detail.objects.get_object_or_404(title='category')
         all_category = Product.filter(detail_id=category)
-        
+        context={
+            'all_category': all_category
+        }
+        return render(request, 'category.html', context)
+
+
