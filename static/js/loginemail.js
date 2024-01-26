c99b22b411d26b87aa1be088386ec5463f355281
fariba_r <faribarezaee321@gmail.com>
2024-01-26 21:38:50 +0330

    chore: some change in products page

--- static/js/loginemail.js
+++ static/js/loginemail.js
@@ -1,3 +1,5 @@
+let cookie = document.cookie
+let csrfToken = cookie.substring(cookie.indexOf('=') + 1)
 window.onload = function() {
     document.getElementById("hided_section").style.display = "none";
 
@@ -8,19 +10,22 @@ document.getElementById("check_email").addEventListener("click", function() {
 
     $.ajax({
     type: "POST",
+
+
     url: `/member/check_email/`,
+         headers: {
+           'X-CSRFToken': csrfToken
+         },
     data: { "email": $("#emaill").val() },
     success: function(data) {
         if (data === "true") {
             document.getElementById("hided_section").style.display = "block";
     document.getElementById("check_email").style.display = "none";
         }
-    }
-});
+    }//end success post
+});//end ajax
 
-}, { passive: false });
-
-
+}, { passive: false });//end lessener
 
 
 
@@ -28,4 +33,6 @@ document.getElementById("check_email").addEventListener("click", function() {
 
 
 
-};
+
+
+};//end load
