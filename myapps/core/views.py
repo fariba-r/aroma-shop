a71e827d1b8494f6d5ec316dfe4c611578498589
fariba_r <faribarezaee321@gmail.com>
2024-01-31 01:28:40 +0330

    chore:create the view of index page

--- myapps/core/views.py
+++ myapps/core/views.py
@@ -1,17 +1,26 @@
+from datetime import timedelta
+
 from django.core.cache import cache
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic import ListView, View, CreateView, TemplateView, FormView, UpdateView
 # Create your views here.
 from ..product.models import *
 class Index(View):
     def get(self, request):
-        def get_context_data(self, **kwargs):
+            context={}
+            for cat in Category.objects.all():
+                if cat.childrn() != None:
+                    cache.set(cat.title, cat.childrn())
 
-            context = super().get_context_data(**kwargs)
-            for cat in Category.objects.all:
-                cache.set(cat.title, cat.childrn())
-            context['new_products'] =Po
+            five_days_ago = timezone.now() - timedelta(days=5)
+            context['new_products'] =Product.objects.filter(created_at__gte=five_days_ago)
+            context['cheapest']=Detail.objects.filter(name='cost').order_by('value')[:30]
+
+            print("c"*50,cache._cache.keys())
+            return render(request,"core/index.html",context)
 
 
-
-
+permission_unlogin=["view_comment","view_detail","view_product","view_category","view_like","view_image"]
+permission_login=["delete_comment","change_comment","add_comment","view_order","add_order","change_order","delete_order",]
+permission_logout=[]
