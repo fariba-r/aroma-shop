ea4aa945b4070e86e5fd1e80e280c9489be60f29
faribarezaee <faribarezaee321@gmail.com>
2024-02-21 00:17:48 +0330

    chore:create hard delete and archive method for manager

--- myapps/member/manager.py
+++ myapps/member/manager.py
@@ -1,33 +1,40 @@
+from django.db import models
+from rest_framework.decorators import api_view, permission_classes
 
-from django.contrib.auth.models import BaseUserManager
-from ..core.manager import CustomBaseManager,DeleteMixin
 
-class CustomUserManager(DeleteMixin,CustomBaseManager,BaseUserManager):
-    def create_user(self, firstname,lastname,phone, email,username, password, **extra_fields):
-        """
-        Creates and saves a User with the given phonenumber,email and password.
-        """
-        if not username or not password:
-            raise ValueError("Users must have username and password")
+class LogicalQuerySet(models.QuerySet):
 
-        user = self.model(first_name=firstname,last_name=lastname,phonenumber=phone,email= email,username=username, password=password, **extra_fields)
-        user.set_password(password)
-        user.is_active = False
-        user.save(using="default")
+    def delete(self):
+        return super().update(is_deleted=True)
 
-        return user
+    def hard_delete(self):
+        return super().delete()
 
-    def create_superuser(self,first_name,last_name,phonenumber, email,username, password=None, **extra_fields):
-        """
-        Creates and saves a superuser with the given phonenumber, nickname, email and password.
-        """
-        user = self.create_user(
-           firstname=first_name,lastname=last_name,phone=phonenumber,email= email,username=username, password=password, **extra_fields
-        )
-        user.is_admin = True
-        user.is_superuser=True
-        user.is_active=True
-        user.is_staff = True
-        user.set_password(password)
-        user.save(using="default")
-        return user
+
+class CustomBaseManager(models.Manager):
+    def get_queryset(self):
+        return LogicalQuerySet(self.model).filter(is_deleted=False)
+
+    def archive(self):
+        return LogicalQuerySet(self.model)
+
+    def deleted(self):
+        return LogicalQuerySet(self.model).filter(is_deleted=True)
+
+
+class DeleteMixin():
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.save()
+
+    def hard_delete(self):
+        super().delete()
+
+    def undelete(self):
+        self.is_deleted = False
+        self.save()
+
+
+
+
+
